d7d49d5ad46bbc7f099e251f15e7b996cb1ae403
farydwm <faryd.w.m@gmail.com>
2024-11-27 19:45:56 -0500

    first commit faryd walteros

---
+++ Ejercicios.py
@@ -0,0 +1,10 @@
+#faryd walteros primer commit
+print ("Recuerda que una vaquita necesita 5 metros cuadrados")
+V = int(input("Ingrese el número de vacas: "))
+A = int(input("Ingrese el número de aves: "))
+K = 5 
+X = float(input("Ingrese los litros de leche que produce tus vacas por día (X): "))
+# Datos de las aves
+mes_dias = 30  # número de días en un mes
+gallinas = A // 3  # 1/3 de las aves son gallinas
+mitad_gallinas = gallinas // 2  # la mitad de las gallinas ponen huevos cada 3 días
